7681b4929b6d581e49e95b60a6c847379b57957c
Voovs Selin <Voovs.13@gmail.com>
2019-12-20 15:49:46 -0700

    Finished battle_ship.rs
    # Finished game
    - All common errors are handeled or resolved
    - Normal run through the game works, without any problems
    - Screen transitions had become much less jarring
    
    ## reuse::pause_for_handoff(str)
    - fn to stop game, until player hits enter
    - Promots user with arugment str
    - Slows down animations, to no longer be jarring when presented
    
    ## annouce_win()
    - Gives final game stats: who won, where both players shot, and both players' remaining ships
    
    ### input_to_point_success()
    - Renamed move_to_point_success() to input_to_point_success()
    - Now returns point values, which can imediatly be used as array indices
    
    ### 2d guess arrays
    - Now hold char as base values
    - print_guess_map() takes in char array

--- src/battle_ship.rs
+++ src/battle_ship.rs
@@ -5,24 +5,40 @@ use std::io;
 pub fn main() {
   let mut p1_ships = [[0; 6]; 6];
   let mut p2_ships = [[0; 6]; 6];
-  let mut p1_guesses = [[" "; 6]; 6];
-  let mut p2_guesses = [[" "; 6]; 6];
+  let mut p1_guesses = [[' '; 6]; 6];
+  let mut p2_guesses = [[' '; 6]; 6];
 
   setup::ready_ships_and_intro(&mut p1_ships, &mut p2_ships);
 
   let mut is_p1_turn = true;
 
-  while !is_win() {
+  while !finish::is_win(&p1_ships, &p2_ships) {
       if is_p1_turn {
+          println!("");
+          println!("Player 1, it's your turn to shoot");
+          println!("Your previous guesses:");
+
+          reuse::print_guess_map(&p1_guesses);
+
           run_game::player_move(&mut p1_guesses, &mut p2_ships);
 
+          reuse::pause_for_handoff("Hit enter and pass to player 2");
       }
       else {
+          println!("");
+          println!("Player 2, it's your turn to shoot");
+          println!("Your previous guesses:");
+
+          reuse::print_guess_map(&p2_guesses);
+
           run_game::player_move(&mut p2_guesses, &mut p1_ships);
 
+          reuse::pause_for_handoff("Hit enter and pass to player 1");
       }
       is_p1_turn = !is_p1_turn;
   }
+
+  finish::announce_win(&p1_ships, &p2_ships, &p1_guesses, &p2_guesses);
 }
 
 mod reuse {
@@ -51,7 +67,9 @@ mod reuse {
         println!("  6  {} | {} | {} | {} | {} | {}", g[0][5], g[1][5], g[2][5], g[3][5], g[4][5], g[5][5]);
         println!("");
     }
-    pub fn print_guess_map(grid: &[[str; 6]; 6]) {
+    pub fn print_guess_map(grid: &[[char; 6]; 6]) {
+        use std::io;
+
         let g = grid;
 
         println!("");
@@ -64,19 +82,9 @@ mod reuse {
         println!("  6  {} | {} | {} | {} | {} | {}", g[0][5], g[1][5], g[2][5], g[3][5], g[4][5], g[5][5]);
         println!("");
     }
-    // pub fn is_win(grid: &[[usize; 6]; 6]) -> bool {
-    //     for column in grid {
-    //         for row in column {
-    //             if grid[column][row] == 0 { continue; }
-    //             else { return false; }
-    //         }
-    //     }
-    //
-    //     true
-    // }
-    pub fn move_to_point_success(p_move: &String) -> Option<(usize, usize)> {
+    pub fn input_to_point_success(p_move: &String) -> Option<(usize, usize)> {
         // Prevents slice panic on insufficient user input string length
-        if p_move.len() < 11 { return None; }
+        if p_move.len() < 2 { return None; }
 
         let column = &p_move[0..1];
         let row = &p_move[1..2];
@@ -104,7 +112,7 @@ mod reuse {
             _  => { return None; },
         };
 
-        Some((column_num, row_num))
+        Some((column_num - 1, row_num - 1))
     }
     pub fn slice_off_last_char(string: &String) -> String {
           let string = &string[..(string.len() - 1)].to_string();
@@ -115,6 +123,25 @@ mod reuse {
 
           string.to_owned()
         }
+    pub fn pause_for_handoff(message: &str) {
+        use std::io;
+
+        let mut hit_enter = String::new();
+
+        println!("{}", message);
+
+        io::stdin().read_line(&mut hit_enter)
+        .expect("Didn't read anything");
+
+        print!("\x1B[2J");
+
+        println!(">>> Only hit enter if it's your turn <<<");
+
+        io::stdin().read_line(&mut hit_enter)
+        .expect("Didn't read anything");
+
+        print!("\x1B[2J");
+    }
 }
 
 mod setup {
@@ -127,13 +154,15 @@ mod setup {
             {
                 // print!("{}[2J", 27 as char); // Clears cmd screen
                 println!("It's player 1's turn. Get ready to place your ships");
+                println!("Valid syntax is (a-f)(1-6) (v or h). Ex: a3 v");
                 println!("");
 
                 user_place_ships(&mut p1_ships);
 
                 super::reuse::print_ship_map(&p1_ships);
 
-                println!("Player 1's ships have been placed");
+                println!("Player 1, your ships have been placed");
+                super::reuse::pause_for_handoff("Press enter and hand off to player 2, to continue");
             }
             {
                 // print!("{}[2J", 27 as char); // Clears cmd screen
@@ -144,8 +173,11 @@ mod setup {
 
                 super::reuse::print_ship_map(&p2_ships);
 
-                println!("Player 2's ships have been placed");
+                println!("Player 2, your ships have been placed");
+                super::reuse::pause_for_handoff("Press enter and hand off to player 1, to continue");
             }
+
+
         println!("All ships are now placed. \"The game is on\" — Sherlock");
     }
 
@@ -209,8 +241,8 @@ mod setup {
         grid: &[[usize; 6]; 6])
         -> bool {
         // Prevents slice panic on insufficient user input string length
-        if p_move.len() < 11 {
-            println!("That's not proper syntax for a move. Ex:       a3 vertical");
+        if p_move.len() < 4 {
+            println!("That's not proper syntax for a move. Ex:       a3 v");
             println!("");
             return false;
         }
@@ -220,9 +252,9 @@ mod setup {
         let mut column_num: usize;
         let mut row_num: usize;
 
-        if let Some((c, r)) = super::reuse::move_to_point_success(p_move){
-            column_num = c;
-            row_num = r;
+        if let Some((c, r)) = super::reuse::input_to_point_success(p_move){
+            column_num = c + 1;
+            row_num = r + 1;
         } else {
             println!("That's not a row or column on the grid. Please use a-f and 1-6 for coordinates");
             println!("");
@@ -231,7 +263,7 @@ mod setup {
 
         // Sum of ship length and row/column_num never exceeds 7
         // Checks if tiles are already taken
-        if direction == "horizontal"
+        if direction == "h"
         && (column_num + ship_length) <= 7 {
             for i in 1..(ship_length + 1) {
                 if grid[column_num + i - 2][row_num - 1] == 0 {
@@ -246,7 +278,7 @@ mod setup {
 
             return true;
         }
-        else if direction == "vertical"
+        else if direction == "v"
         && (row_num + ship_length) <= 7 {
             for i in 1..(ship_length + 1) {
                 if grid[column_num - 1][row_num + i - 2] == 0 {
@@ -262,13 +294,13 @@ mod setup {
             return true;
         }
         else if ( (row_num + ship_length) <= 7 || (column_num + ship_length) <= 7 )
-        &&           ( direction == "vertical" || direction == "horizontal" ) {
+        &&                  ( direction == "v" || direction == "h" ) {
             println!("Your ship is literally off the charts (too long for the grid). Double check your math, unless you seriously doubt a computer's processing *lols*");
             println!("");
             return false;
         }
         else {
-            println!("Check the spelling of your direction. It's either:    vertical | horizontal");
+            println!("Check the spelling of your direction. It's either:    v (for vertical) OR h (for horizontal)");
             println!("");
             return false;
         }
@@ -284,16 +316,14 @@ mod setup {
             let mut column_num: usize;
             let mut row_num: usize;
 
-            if let Some((c, r)) = super::reuse::move_to_point_success(&p_move) {
+            if let Some((c, r)) = super::reuse::input_to_point_success(&p_move) {
                 column_num = c;
                 row_num = r;
             } else { return false; }
 
-            let (column_num, row_num) = (column_num - 1, row_num - 1);
-
             let direction = &p_move[3..];
 
-            if direction == "horizontal" {
+            if direction == "h" {
                 for i in 0..(ship_len) {
                     if grid[column_num + i][row_num] == 0 {
                           grid[column_num + i][row_num] = ship_len;
@@ -303,7 +333,7 @@ mod setup {
                     }
                 }
             }
-            else if direction == "vertical" {
+            else if direction == "v" {
                 for i in 0..ship_len {
                     if grid[column_num][row_num + i] == 0 {
                           grid[column_num][row_num + i] = ship_len;
@@ -322,64 +352,70 @@ mod run_game {
     use std::io;
 
     pub fn player_move(
-        guess_grid: &mut [[str; 6]; 6],
-        target_grid: &mut [[usize; 6]; 6])
+        guess_grid: &mut [[char; 6]; 6],
+        mut target_grid: &mut [[usize; 6]; 6])
         -> () {
-            let (column_num, row_num) = get_player_input_point();
-            let (column, row) = (column_num - 1, row_num - 1);
+        let (column, row) = get_player_input_point(&guess_grid);
 
-            if target_grid[column][row] != 0 {
-                guess_grid[column][row] = "X";
+        if target_grid[column][row] != 0 {
+            guess_grid[column][row] = 'X';
 
-                super::reuse::print_guess_map(&guess_grid);
+            println!("Your new guesses:");
 
-                update_target_grid(&mut target_grid, (column, row));
+            super::reuse::print_guess_map(&guess_grid);
 
-            } else {
-                guess_grid[column][row] = "O";
-                println!("You missed");
-            }
+            update_target_grid(&mut target_grid, (column, row));
+
+        } else {
+            guess_grid[column][row] = 'O';
+
+            println!("You missed");
+        }
     }
 
-    fn get_player_input_point() -> (usize, usize) {
+    #[allow(unused_assignments)]
+    fn get_player_input_point(g_grid: &[[char; 6]; 6]) -> (usize, usize) {
         let mut player_input = String::new();
-
-        let mut column: usize;
-        let mut row: usize;
 
         loop {
             player_input = String::new();
 
-            io.stdin().read_line(&mut player_input)
+            io::stdin().read_line(&mut player_input)
             .expect("Didn't read line");
 
-            if player_input.len() == 2 {
+            if player_input.len() == 3 {
                 player_input = super::reuse::slice_off_last_char(&player_input);
             } else {
-                println!("That's not a valid point. Use values a-f and 1-6. Ex: a3");
+                println!("An input is at least 2 characters. Ex: a3");
                 continue;
             }
 
-            if is_valid_target(&player_move) { break; }
+            if is_valid_target(&player_input, &g_grid) { break; }
         }
 
-        if let Some((c, r)) = super::reuse::move_to_point_success(&player_input) {
+        if let Some((c, r)) = super::reuse::input_to_point_success(&player_input) {
             return (c, r);
         } else {
             panic!("Else on if let @ get_player_input_point()");
         }
     }
 
-    fn is_valid_target(input: &String) -> bool {
+    fn is_valid_target(input: &String, g_grid: &[[char; 6]; 6]) -> bool {
         if input.len() < 2 {
             println!("An input is at least 2 characters. Ex: a3");
             return false;
         }
 
-        if let Some((c, r)) = super::reuse::move_to_point_success(input) {
-            return true;
+        if let Some((c, r)) = super::reuse::input_to_point_success(input) {
+            if g_grid[c][r] == ' ' {
+                return true;
+            }
+            else {
+                println!("You've already guesses at that tile. Repeating it won't get you anything");
+                return false;
+            }
         } else {
-            println!("That's not a value on the grid. Please use a-f and 1-6. Ex: a3");
+            println!("That's not a tile on the field. Please use a-f and 1-6. Ex: a3");
             return false;
         }
     }
@@ -396,8 +432,8 @@ mod run_game {
 
         grid[c][r] = 0;
 
-        for column in grid {
-            for row in column {
+        for column in 0..6 {
+            for row in 0..6 {
                 if grid[column][row] == ship_num {
                     return;
                 }
@@ -414,17 +450,17 @@ mod finish {
         p2_grid: &[[usize; 6]; 6])
         -> bool {
 
-        'p1_win: for column in p1_grid {
-            for row in column {
+        'p1_win: for column in 0..6 {
+            for row in 0..6 {
                 if p1_grid[column][row] != 0 {
-                    break 'p1 win;
+                    break 'p1_win;
                 }
             }
             if column == 5 { return true; }
         }
 
-        'p2_win: for column in p2_grid {
-            for row in column {
+        'p2_win: for column in 0..6 {
+            for row in 0..6 {
                 if p2_grid[column][row] != 0 {
                     break 'p2_win;
                 }
@@ -434,171 +470,66 @@ mod finish {
 
         false
     }
+
+    pub fn announce_win(
+        p1_ships: &[[usize; 6]; 6],
+        p2_ships: &[[usize; 6]; 6],
+        p1_guesses: &[[char; 6]; 6],
+        p2_guesses: &[[char; 6]; 6])
+        -> () {
+        let mut win_player_num = ' ';
+        let mut lose_player_num = ' ';
+
+        'win_p_num: for column in 0..6 {
+            for row in 0..6 {
+                if p1_ships[column][row] != 0 {
+                    println!("P_1 win on column: {} | row: {}", column, row);
+                    win_player_num = '1';
+                    lose_player_num = '2';
+                    break 'win_p_num;
+                }
+            }
+            if column == 5 {
+                println!("P_2 win on column: {}", column);
+                win_player_num = '2';
+                lose_player_num = '1';
+            }
+        }
+
+
+        println!("Well done player {}! Your skills, with absolutely no luck, have allowed you to shoot down all player {} ships. You can now relax, after these potent graphics have left nothing to the imagination", win_player_num, lose_player_num);
+        pause_no_handoff();
+
+        println!("Here are your final fields");
+        println!("Player 1's surviving ships:");
+        super::reuse::print_ship_map(&p1_ships);
+        pause_no_handoff();
+
+        println!("Player 2's surviving ships");
+        super::reuse::print_ship_map(&p2_ships);
+        pause_no_handoff();
+
+        println!("Player 1's guesses on the field");
+        super::reuse::print_guess_map(&p1_guesses);
+        pause_no_handoff();
+
+        println!("Player 2's guesses on the field");
+        super::reuse::print_guess_map(&p2_guesses);
+        println!("");
+
+        println!("Thanks for playing!!!");
+    }
+
+    fn pause_no_handoff() {
+        use std::io;
+
+        let mut hit_enter = String::new();
+
+        println!("");
+
+        println!("Press enter to continue:");
+
+        io::stdin().read_line(&mut hit_enter)
+        .expect("Didn't read anything");
+    }
 }
-
-
-
-
-// fn create() {
-//
-// }
-//
-// fn game_setup() -> (Grid_Tiles, Grid_Tiles) {
-//     let mut p1_ship = user_place_ships();
-//     let mut p2_ship = user_place_ships();
-//
-//     (p1_ship, p2_ship)
-// }
-//
-// fn user_place_ships() -> Grid_Tiles {
-//     let mut ship_2_place = String::new();
-//     let mut ship_3_place = String::new();
-//     let mut ship_4_place = String::new();
-//
-//     loop {
-//         if !is_valid_move(ship_2_place, 2) {
-//             println!("Place your 2 tile ship (X X)");
-//
-//             io::stdin().read_line(&mut ship_2_place)
-//             .expect("Didn't read anything");
-//
-//             if is_valid_move(ship_2_place, 2) { continue; }
-//         }
-//         else if !is_valid_move(ship_3_place, 3) {
-//             println!("Place your 3 tile ship (X X X)");
-//
-//             io::stdin().read_line(&mut ship_3_place)
-//             .expect("Didn't read anything");
-//
-//             if is_valid_move(ship_3_place, 3) { continue; }
-//         }
-//         else if !is_valid_move(ship_4_place, 4) {
-//             println!("Place your 4 tile ship (X X X X)");
-//
-//             io::stdin().read_line(&mut ship_4_place)
-//             .expect("Didn't read anything");
-//
-//             if is_valid_move(ship_4_place, 4) { continue; }
-//         }
-//         else { break; }
-//
-//         // Will only print if the user's placement was invalid
-//         println!("");
-//         println!("That isn't a valid move, or has improper syntax");
-//         println!("An example of valid sytax is:   a3 vertical");
-//         println!("");
-//     }
-//
-//     let place_array = [ship_2_place, ship_3_place, ship_4_place];
-//
-//     for ship in place_array.iter() {
-//
-//     }
-//
-//
-// }
-//
-// fn is_valid_move(user_ship_place: String, ship_length: u8) -> bool {
-//     let column = &user_ship_place[0..1];
-//     let row = &user_ship_place[1..2];
-//     let direction = &user_ship_place[3..];
-//
-//     if column == "a" || column == "b" || column == "c"
-//     || column == "d" || column == "e" || column == "f"
-//     && row == "1" || row == "2" || row == "3"
-//     || row == "4" || row == "5" || row == "6"
-//     && direction == "vertical" || direction == "horizontal" {
-//         match ship_length {
-//             "2" => {
-//                 if direction == "vertical"
-//                 && row != "6"
-//                 { return true; }
-//
-//                 else if direction == "horizontal"
-//                 && column != "f"
-//                 { return true; }
-//
-//                 else { return false; }
-//             },
-//             "3" => {
-//                 if direction == "vertical"
-//                 && row != "6" && row != "5"
-//                 { return true; }
-//
-//                 else if direction == "horizontal"
-//                 && column != "f" && column != "e"
-//                 { return true; }
-//
-//                 else { return false; }
-//             },
-//             "4" => {
-//                 if direction == "vertical"
-//                 && row != "6" && row != "5" && row != "4"
-//                 { return true; }
-//
-//                 else if direction == "horizontal"
-//                 && column != "f" && column != "e" && column != "d"
-//                 { return true; }
-//
-//                 else { return false; }
-//             },
-//              _  => panic!("Error on ship length @is_valid_move()");,
-//         }
-//     }
-//     else {
-//         return false;
-//     }
-// }
-
-//   A   B   C   D   E   F
-// 1   |   |   |   |   |
-// 2   |   |   |   |   |
-// 3   |   |   |   |   |
-// 4   |   |   |   |   |
-// 5   |   |   |   |   |
-// 6   |   |   |   |   |
-
-// ship 2: (X X)
-// ship 3: (X X X)
-// ship 4: (X X X X)
-
-
-
-// let mut p2_guess = GridTiles {
-//     a1: false,
-//     b1: false,
-//     c1: false,
-//     d1: false,
-//     e1: false,
-//     f1: false,
-//     a2: false,
-//     b2: false,
-//     c2: false,
-//     d2: false,
-//     e2: false,
-//     f2: false,
-//     a3: false,
-//     b3: false,
-//     c3: false,
-//     d3: false,
-//     e3: false,
-//     f3: false,
-//     a4: false,
-//     b4: false,
-//     c4: false,
-//     d4: false,
-//     e4: false,
-//     f4: false,
-//     a5: false,
-//     b5: false,
-//     c5: false,
-//     d5: false,
-//     e5: false,
-//     f5: false,
-//     a6: false,
-//     b6: false,
-//     c6: false,
-//     d6: false,
-//     e6: false,
-//     f6: false,
-// };
